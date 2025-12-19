86cd3911bed1455251f3966dcd4df6858e96adc9
Pedro Henrique <pedro.lg.cs@gmail.com>
2025-12-19 14:14:40 -0300

    .

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,19 +1,16 @@
 import { Image, View, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 import { CreateTodo } from '@/modules/todo/create-todo';
 import { ListTodo } from '@/modules/todo/list-todo';
 import { images } from '@/assets';
 
-export function Home() {
+export default function Home() {
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View style={styles.container}>
-        <Image source={images.logo} style={styles.logo} />
-        <CreateTodo />
-        <ListTodo />
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <Image source={images.logo} style={styles.logo} />
+      <CreateTodo />
+      <ListTodo />
+    </View>
   );
 }
 
@@ -23,7 +20,7 @@ const styles = StyleSheet.create({
     gap: 30,
     alignItems: 'center',
     backgroundColor: '#d4d4d4',
-    paddingTop: 30,
+    paddingTop: 24,
   },
   logo: {
     height: 34,
